3536a2a8aaa107b4748b553825ec40ce02438e6c
noemiostorga <noemiostorga@gmail.com>
2015-11-05 12:00:27 -0800

    console.log points

---
+++ functions.js
@@ -0,0 +1,26 @@
+
+	var deathStar =function () {
+		this.health;
+		this.points;
+		this.player;
+	}
+
+	var planets = function (){
+		this.UniquePoints;
+	}
+
+$('#planets').click(function (){
+
+	this.UniquePoints = console.log( Math.ceil(Math.random()*1000 + 1) );
+});
+
+
+
+
+//on click replace the planets with points
+	//1. make a click function on planet
+		//1.1 on click mke planet disapear
+	//2.then make 'points ' show up
+
+
+
