564822f0ae5923de2fad054199c45ee2be52c7e5
noemiostorga <noemiostorga@gmail.com>
2015-11-06 16:04:07 -0800

    added overlay button

--- functions.js
+++ functions.js
@@ -1,82 +1,78 @@
+$(document).ready(function (){
+
+
 var deathEstrella = function() {
-	this.punto = points ; //function that'll keep on adding points
+	this.punto = points; //function that'll keep on adding points
 	this.salud = 100;
-	this.planeta = planeta;
-	this.ataque = attack;
+	this.planet = planet;
+	this.ataque = attack; //function that'll attack and affect health
 }
 
 
 
-//1.make an overlay so when clicked 
+//1.make overlay so when clicked 
 	//fades into the game
+var overlay = function() {
+	overlay.appendto(".container");
+}
 
 
 
-
-
-$("#planets").on("click", function attack() {
-	//When clicked on planets 
-	//1. make a click function on planet
-		//1.1 make planet disapear
-	$("#planets").toggleClass("replacePlanets");
-	
+	$("#planets").on("click", function attack() {
+		//When clicked on planets 
+		//1. make a click function on planet
+			//1.1 make planet disapear
+		$("#planets").toggleClass("replacePlanets");
 		
-	//2.then make 'points ' show up/replace
-	 //--replance planets should already have score points
+			
+		//2.then make 'points ' show up/replace
+		 //--replance planets should already have score points
 
-	/*make other planets reapear once this planet disappears
-		//have planet *slide* in once the other planet is gone
- 	*/
-	console.log("planets")
-});
-
-var attack = new deathEstrella('shoot');
-deathEstrella.shoot = function (){
-
-}
+		/*make other planets reapear once this planet disappears
+			//have planet *slide* in once the other planet is gone
+	 	*/
+		console.log("planets")
+	});
 
 
 
-planeta.replacePlanets = function (attack) {
-	return console.log("cst");
-}
 
 
-var deathEstrella = function () {
-	//have image fade little by three phases
-	//1.make image fade 2/3 health
-	//2.make image opacity fade more at amost 1/3 health
+	var deathEstrella = function () {
+		//have image fade little by three phases
+		//1.make image fade 2/3 health
+		//2.make image opacity fade more at amost 1/3 health
 
-		/*shot green array on planet when click on planet 
-			//add sounds when button is click 
-		*/
-	console.log()
-}
+			/*shot green array on planet when click on planet 
+				//add sounds when button is click 
+			*/
+		console.log()
+	}
 
-var punto = function() {
-	//math.random will be points on planets range from 0 - 100
-	
-	//1.make number go on score board 
-		//added current score onto the past schore numbers
+	var punto = function() {
+		//math.random will be points on planets range from 0 - 100
+		
+		//1.make number go on score board 
+			//added current score onto the past schore numbers
 
-	//3.replace planet clicked on with the score that has been given 
-		//if *bad # * make color of font red  
+		//3.replace planet clicked on with the score that has been given 
+			//if *bad # * make color of font red  
 
-	
+		
 
-}
+	}
 
-var salud = function () {
-	//1.if '0' 
-		//health will go down by 10%
+	var salud = function () {
+		//1.if '0' 
+			//health will go down by 10%
 
-		/*if number from the movies make *critical hit* 
-		 which brings health down to 30% */
+			/*if number from the movies make *critical hit* 
+			 which brings health down to 30% */
 
-	//2. change color of bar 2/3
-	//3.change color in 1/3
+		//2. change color of bar 2/3
+		//3.change color in 1/3
 
-}
+	}
 
 
 
@@ -84,7 +80,7 @@ var salud = function () {
 
 
 
-
+}) //closed doc.ready 
 
 
 
