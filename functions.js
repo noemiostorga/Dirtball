8929e3797630695021e651f4d41f1a637239819c
noemiostorga <noemiostorga@gmail.com>
2015-11-08 15:40:41 -0800

    fixed overlay

--- functions.js
+++ functions.js
@@ -1,20 +1,40 @@
 $(document).ready(function (){
 
 
+$("#Btn").on("click", function(){;
+	$(".overlay").fadeOut();
+})
+
+
 var deathEstrella = function() {
 	this.punto = points; //function that'll keep on adding points
 	this.salud = 100;
 	this.planet = planet;
-	this.ataque = attack; //function that'll attack and affect health
+	this.attack = function (opponent) {
+	    var hitChance = Math.random();
+	    if (hitChance > .6) {
+	      planet.health -= 10;
+	    } else {
+	      console.log(" missed!!!!");
+	    }
+	  };
+	  this.criticalHit = function (opponent) {
+	    opponent.health -= this.randomCrit();
+	  };
+	  this.randomCrit = function () {
+	    var max = 20;
+	    var min = 11;
+
+	    return Math.floor(Math.random() * (max - min) + min);
+	  }; //function that'll attack and affect health
 }
 
 
 
 //1.make overlay so when clicked 
 	//fades into the game
-var overlay = function() {
-	overlay.appendto(".container");
-}
+//2. make other stuff hide
+
 
 
 
@@ -23,15 +43,17 @@ var overlay = function() {
 		//1. make a click function on planet
 			//1.1 make planet disapear
 		$("#planets").toggleClass("replacePlanets");
-		
+		$("#points").text(punto());
+	
 			
 		//2.then make 'points ' show up/replace
 		 //--replance planets should already have score points
 
+
 		/*make other planets reapear once this planet disappears
 			//have planet *slide* in once the other planet is gone
 	 	*/
-		console.log("planets")
+		console.log("You've attack!");
 	});
 
 
@@ -46,19 +68,17 @@ var overlay = function() {
 			/*shot green array on planet when click on planet 
 				//add sounds when button is click 
 			*/
-		console.log()
+		
 	}
 
 	var punto = function() {
 		//math.random will be points on planets range from 0 - 100
-		
+		return Math.ceil((Math.random() * (10 - 0) + 0));
 		//1.make number go on score board 
 			//added current score onto the past schore numbers
 
 		//3.replace planet clicked on with the score that has been given 
-			//if *bad # * make color of font red  
-
-		
+			//if *bad # * make color of font red  	
 
 	}
 
@@ -79,8 +99,7 @@ var overlay = function() {
 
 
 
-
-}) //closed doc.ready 
+}); //closed doc.ready 
 
 
 
